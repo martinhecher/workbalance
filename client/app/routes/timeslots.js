9376c13c765588a0aaeb4479c5228a3de146b441
Martin Hecher <martin.hecher@fraunhofer.at>
2015-01-18 18:34:54 +0100

    added listing of timeslots on client

--- client/app/routes/timeslots.js
+++ client/app/routes/timeslots.js
@@ -1,4 +1,12 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+	model: function() {
+		return this.store.find('timeslot');
+	},
+
+	setupController: function(controller, model) {
+		controller.set('timeslots', model); // transform the model into something semantically richer...
+		controller.set('model', model);
+	}
 });
